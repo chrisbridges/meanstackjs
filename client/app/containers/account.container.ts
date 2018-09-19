508ad3eefde5a1644d55f48018c2bcefcd603008
chris.bridges <christopher_bridges@icloud.com>
2018-09-19 13:29:08 -0400

    tasks added before trying to configure proper routing

--- client/app/containers/account.container.ts
+++ client/app/containers/account.container.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { UserService, AlertService } from '../services';
-
+// TODO: need to get routing for component squared away
 @Component({
   selector: 'app-account',
   template: `
@@ -9,17 +9,13 @@ import { UserService, AlertService } from '../services';
       <h2>Profile Information</h2>
       <form class="form-account" [formGroup]="accountForm" (ngSubmit)="onSubmit()">
         <div class="form-group">
-          <label for="exampleInputEmail1">Email</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <label for="account-email">Email</label>
+          <input type="email" class="form-control" id="account-email" aria-describedby="emailHelp" placeholder="Enter email">
           <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-        <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          <label for="account-name">Name</label>
+          <input type="text" class="form-control" id="account-name" placeholder="Name">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
@@ -37,10 +33,10 @@ export class AccountContainer {
   ) {}
 
   ngOnInit () {
-
+    // TODO: grab current account values and fill inputs accordingly
   }
 
   onSubmit () {
-    
+    // TODO: if values have changed, make PUT request
   }
 }
