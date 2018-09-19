4f809002ad027ad72e4d46064050c0101f80bcff
chris.bridges <christopher_bridges@icloud.com>
2018-09-19 14:33:13 -0400

    inputs properly set up on rows - moving to radio buttons

--- client/app/containers/account.container.ts
+++ client/app/containers/account.container.ts
@@ -1,29 +1,55 @@
 import { Component, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { UserService, AlertService } from '../services';
-// TODO: need to get routing for component squared away
+
 @Component({
   selector: 'app-account',
   template: `
     <div class="container">
       <h2>Profile Information</h2>
       <form class="form-account" [formGroup]="accountForm" (ngSubmit)="onSubmit()">
-        <div class="form-group">
-          <label for="account-email">Email</label>
-          <input type="email" class="form-control" id="account-email" aria-describedby="emailHelp" placeholder="Enter email">
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <div class="form-group row">
+          <label for="account-email" class="col-form-label col-sm-2">Email</label>
+          <div class="col-sm-6">
+            <input type="email" class="form-control" id="account-email" aria-describedby="emailHelp">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
         </div>
-        <div class="form-group">
-          <label for="account-name">Name</label>
-          <input type="text" class="form-control" id="account-name" placeholder="Name">
+        <div class="form-group row">
+          <label for="account-name" class="col-form-label col-sm-2">Name</label>
+          <div class="col-sm-6">
+            <input type="text" class="form-control" id="account-name">
+          </div>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="form-check row">
+          <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+          <label class="form-check-label" for="exampleRadios1">
+            Male
+          </label>
+          <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+          <label class="form-check-label" for="exampleRadios2">
+            Female
+          </label>
+        </div>
+        <div class="form-group row">
+          <label for="account-location" class="col-form-label col-sm-2">Location</label>
+          <div class="col-sm-6">
+            <input type="text" class="form-control" id="account-location">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="account-website" class="col-form-label col-sm-2">Website</label>
+          <div class="col-sm-6">
+            <input type="text" class="form-control" id="account-website">
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary"><i class="fa fa-pencil"></i> Update Profile</button>
       </form>
     </div>
   `
 })
 
-export class AccountContainer {
+export class AccountContainer implements OnInit {
   accountForm: FormGroup
 
   constructor (
