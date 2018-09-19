e3a05c60abed7bf8f642cbe30f57777fc37ce91a
chris.bridges <christopher_bridges@icloud.com>
2018-09-19 12:29:50 -0400

    getting boilerplate set up - getting no provider for controlContainer error though

--- client/app/containers/account.container.ts
+++ client/app/containers/account.container.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { UserService, AlertService } from '../services';
 
 @Component({
-  selector: 'app-acount',
+  selector: 'app-account',
   template: `
     <div class="container">
-        ACCOUNT
+      <h2>Profile Information</h2>
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
   `
 })
-export class AccountContainer { }
+
+export class AccountContainer {
+  accountForm: FormGroup
+
+  constructor (
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private alertService: AlertService
+  ) {}
+
+  ngOnInit () {
+
+  }
+}
